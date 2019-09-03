80007bb27e603d7d08abe6e5c5772126b56d57b0
Cxx0822 <1556464090@qq.com>
2019-09-03 11:54:46 +0800

    Create Model

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ from tensorboardX import SummaryWriter
 from model import Models
 from load_data import * 
 import yaml
+import time
+import datetime
 
 with open("info.yml") as stream:
     my_data = yaml.load(stream, Loader=yaml.FullLoader)
@@ -24,11 +26,13 @@ writer = SummaryWriter()
 
 
 def my_train():
+    # 是否有GPU加速
     use_gpu = torch.cuda.is_available()
 
     dataloader = {"train": load_data(data_dir_train, image_size, batch_size), 
                   "valid": load_data(data_dir_valid, image_size, batch_size)}
 
+    # 加载模型、损失、优化器等
     model = Models(image_dim, n_classes)
     loss_f = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -39,52 +43,41 @@ def my_train():
     for epoch in range(epochs):
         print("Epoch {}/{}".format(epoch + 1, epochs))
         print("-" * 10)
+
         running_loss = 0.0
         train_correct = 0
         train_total = 0
+
+        # 计时
+        start_time = time.time()
+
         for i, data in enumerate(dataloader["train"], 0):
-            inputs, train_labels = data
+            train_images, train_labels = data
             if use_gpu:
-                inputs, labels = Variable(inputs.cuda()), Variable(train_labels.cuda())
+                train_images, labels = Variable(train_images.cuda()), Variable(train_labels.cuda())
             else:
-                inputs, labels = Variable(inputs), Variable(train_labels)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            _, train_predicted = torch.max(outputs.data, 1)
+                train_images, labels = Variable(train_images), Variable(train_labels)
+
+            optimizer.zero_grad()                              # 梯度置零
+            outputs = model(train_images)                      # 模型输出
+            _, train_predicted = torch.max(outputs.data, 1)    # 预测结果
+
             train_correct += (train_predicted == labels.data).sum()
-            loss = loss_f(outputs, labels)
-            loss.backward()
-            optimizer.step()
+
+            loss = loss_f(outputs, labels)    # 计算损失
+            loss.backward()                   # 反向传播
+            optimizer.step()                  # 单步优化（调整学习率）
+
             running_loss += loss.item()
             train_total += train_labels.size(0)
 
+        now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        runtime = time.time() - start_time
+        print('now_time: %s, , time/epoch:%.2fs, time left: %.2fhours' % (str(now_time), runtime, (epochs - epoch) * runtime / 3600))
         print('train %d epoch loss: %.3f  acc: %.3f ' % (epoch + 1, running_loss / train_total, 100 * train_correct / train_total))
+
         writer.add_scalar('Train/Loss', running_loss / train_total, epoch + 1)
         writer.add_scalar('Train/Acc', 100 * train_correct / train_total, epoch + 1) 
-
-        # 模型测试
-        correct = 0
-        test_loss = 0.0
-        test_total = 0
-        test_total = 0
-        model.eval()
-        for data in dataloader["valid"]:
-            images, labels = data
-            if use_gpu:
-                images, labels = Variable(images.cuda()), Variable(labels.cuda())
-            else:
-                images, labels = Variable(images), Variable(labels)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            loss = loss_f(outputs, labels)
-            test_loss += loss.item()
-            test_total += labels.size(0)
-            correct += (predicted == labels.data).sum()
-
-        print('test  %d epoch loss: %.3f  acc: %.3f ' % (epoch + 1, test_loss / test_total, 100 * correct / test_total))
-        writer.add_scalar('Test/Loss', test_loss / test_total, epoch + 1)
-        writer.add_scalar('Test/Acc', 100 * correct / test_total, epoch + 1)
-    writer.close()
 
     torch.save(model, 'model.pt')
 
